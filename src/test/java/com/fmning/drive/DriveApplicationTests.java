0dab5f31d01cb10969ebf4fc9c5fb7e81b444c8b
synfm@126.com <synfm@126.com>
2021-03-04 22:26:28 -0600

    Init Gradle project

---
+++ src/test/java/com/fmning/drive/DriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmning.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
